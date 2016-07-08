f45155c5887affa6f7a999f0760699714e03fef3
andela-tolotin <temitope.olotin@andela.com>
2016-07-08 10:41:45 +0100

    Create add product method

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Inventory\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Inventory\Http\Requests;
+use Inventory\Product;
+Inventory\Http\Requests\ProductRequest;
+
+class ProductController extends Controller
+{
+    public function addProduct(ProductRequest $request)
+    {
+        $product = Product::create([
+            'name' => $request->input('name'),
+            'price' => $request->input('price'),
+            'category_id' => $request->input('category'),
+        ]);
+
+        if (!is_null($product)) {
+            return rediect('/add-product')->with('status', 'Product Added Successfully');
+        }
+
+        return rediect('/add-product')->with('status', 'Product Not Added Successfully');
+    }
+}
