6e24e796f37ac596ac93e81c30e74da59e8b5623
andela-tolotin <temitope.olotin@andela.com>
2016-07-08 10:42:03 +0100

    Create category controller

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Inventory\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Inventory\Http\Requests;
+use Inventory\Category;
+
+class CategoryController extends Controller
+{
+    public function getProductCategories()
+    {
+        
+    }
+}
