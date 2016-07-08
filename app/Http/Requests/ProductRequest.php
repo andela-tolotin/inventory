a78a0f06e2dea2772b54f9925bce752e80f83767
andela-tolotin <temitope.olotin@andela.com>
2016-07-08 10:40:28 +0100

    Add product request

---
+++ app/Http/Requests/ProductRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Inventory\Http\Requests;
+
+use Inventory\Http\Requests\Request;
+
+class ProductRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|max:50|unique:products',
+            'price' => 'required|max:5',
+            'category' => 'required',
+        ];
+    }
+}
