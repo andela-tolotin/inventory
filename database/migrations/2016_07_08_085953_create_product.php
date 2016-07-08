860d711a9deff2f1f308323db911967d9391d5c4
andela-tolotin <temitope.olotin@andela.com>
2016-07-08 10:42:26 +0100

    Modified products schema

--- database/migrations/2016_07_08_085953_create_product.php
+++ database/migrations/2016_07_08_085953_create_product.php
@@ -16,7 +16,7 @@ class CreateProduct extends Migration
             $table->increments('id');
             $table->string('name');
             $table->integer('price');
-            $table->integer('category_id')->unsigned->default(1);
+            $table->integer('category_id')->unsigned()->default(1);
             $table->timestamps();
 
             $table->foreign('categories')
