e1ca769afcf4c48b0f2fb720ee80edd2d11fbc0d
andela-tolotin <temitope.olotin@andela.com>
2016-07-08 10:01:40 +0100

    Adjust product form

--- resources/views/page/add_product.blade.php
+++ resources/views/page/add_product.blade.php
@@ -1,6 +1,17 @@
 @extends('master')
 @section('content')
 <div>
-<h1>Welcome to Add Product Page</h1>
+    <h4>Welcome to Add Product Page</h4>
+    <div class="product_form">
+        <form action="/product/create" method="POST">
+            <input type="hidden" name="_token" value="{{  csrf_token()  }}">
+            Category : <select name="category">
+                <option>Choose</option>
+            </select> <br/>
+            Name: <input type="text" name="name" value="{{ old('name')}}"><br/>
+           Price:  <input type="text" name="price" value="{{ old('price') }}"> <br/>
+            <button type="submit">Add</button>
+        </form>
+    </div>
 </div>
 @endsection
